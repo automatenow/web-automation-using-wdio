bb7efb086585ff16a532e88d4d65dc9b5d7c7072
automateNow <info@automatenow.io>
2024-02-18 16:28:46 -0500

    added a beforeEach hook to login.spec.js

--- src/test/login.spec.js
+++ src/test/login.spec.js
@@ -1,8 +1,11 @@
 import LoginPage from '../page_objects/login.page.js'
 
 describe('Test login', () => {
-    it('logs in using valid credentials', async () => {
+    beforeEach(async function () {
         await LoginPage.open()
+    })
+
+    it('logs in using valid credentials', async () => {
         await LoginPage.login("happy-tester@test.com", "password")
         await browser.pause(3000)
     })
