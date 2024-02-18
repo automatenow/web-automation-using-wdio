a9916076bc30fbcf7d8e65adb29235c8b652c31a
automateNow <info@automatenow.io>
2024-02-17 23:04:18 -0500

    Added browserVersion to circumvent an issue with Chrome not launching. Also did some POM refactoring.

---
+++ src/test/home.spec.js
@@ -0,0 +1,43 @@
+describe('Introduction to wdio basic features', () => {
+    it('searches for a product', async () => {
+        browser.url('https://ecommerce-playground.lambdatest.io/')
+        // await $('input[name="search"]').setValue("iphone")
+        // await $('button[type="submit"]').click()
+        // await browser.pause(3000)
+
+        // Element hover
+        await $('//span[normalize-space()="Mega Menu"]').moveTo()
+        await $('a[title="Headphones"]').click()
+        browser.pause(3000)
+    })
+
+    it('uses the waitUntil command', async () => {
+        browser.url('https://ecommerce-playground.lambdatest.io/')
+        browser.maximizeWindow()
+
+        await $('//span[normalize-space()="Mega Menu"]').moveTo()
+        await $('a[title="Printer"]').click()
+
+        const title = await $('h1')
+        await title.waitUntil(async function() {
+            return await this.getText() === 'Printers'
+        })
+
+        async function numberOfResultsToBe(count){
+            const elementCount = $$('.product-thumb').length
+            return await elementCount === count 
+        }
+
+        const checkbox = await $('(//label[normalize-space()="Pre-Order"])[2]')
+        await checkbox.click()
+        await browser.waitUntil(async function() {
+            return await numberOfResultsToBe(2)
+        },
+        {
+            timeout: 3000,
+            timeoutMsg: 'did not find corrent # of results',
+            interval: 1000
+        })
+        browser.pause(3000)
+    })
+})
