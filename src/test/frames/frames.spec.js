d3341a6330a7734138266f62c8c2dce63c15e5ce
automateNow <info@automatenow.io>
2024-07-11 21:16:27 -0400

    Updated frames tests

--- src/test/frames/frames.spec.js
+++ src/test/frames/frames.spec.js
@@ -4,26 +4,25 @@ describe('Working with frames', () => {
         await browser.url('https://www.lambdatest.com/selenium-playground/nested-frames/');
         
         // Switch to the bottom frame
-        const bottomFrame = await browser.$('frame[name="frame-bottom"]');
+        const bottomFrame = await $('frame[name="frame-bottom"]');
         await browser.switchToFrame(bottomFrame);
 
         // Switch to the middle frame within the bottom frame
-        const middleFrame = await browser.$('frame[name="frame-middle"]');
+        const middleFrame = await $('frame[name="frame-middle"]');
         await browser.switchToFrame(middleFrame);
 
         // Find the text "Middle" in the bottom middle frame
-        let text = await browser.$('body').getText();
+        let text = await $('body').getText();
         expect(text).toBe('Middle');
 
         // Switch to default frame
-        await browser.switchToFrame(null)
+        await browser.switchToFrame(null);
 
-        // Switch to top frame
-        const topFrame = await browser.$('frame[name="frame-top"]');
+        const topFrame = await $('frame[name="frame-top"]');
         await browser.switchToFrame(topFrame);
 
         // Find the text "Top" in the top frame
-        text = await browser.$('body').getText();
+        text = await $('body').getText();
         expect(text).toBe('Top');
     });
 
@@ -31,15 +30,15 @@ describe('Working with frames', () => {
         await browser.url('https://practice-automation.com/iframes/');
 
         // Switch to the top iframe
-        const iframe = await $('#iframe-1');
-        await browser.switchToFrame(iframe);
+        const topIframe = await $('#iframe-1');
+        await browser.switchToFrame(topIframe);
 
-        // Click 'Get Started'
-        const getStarted = await $('a[href="/docs/intro"]')
-        getStarted.click();
+        // Click 'Docs'
+        const docs = await $('a[href="/docs/intro"]');
+        docs.click();
 
-        // Verify the page title
-        const pageTitle = await browser.$('h1').getText();
-        expect(pageTitle).toBe('Installation');
+         // Verify title
+        const title = await $('h1').getText();
+        expect(title).toBe('Installation')
     });
 });
